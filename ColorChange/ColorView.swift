398ed323596b27162b82bf07c2431c5064e6d51b
walbeckcooper <walbeckcooper@gmail.com>
2022-08-24 12:16:45 -0600

    Initial commit

---
+++ ColorChange/ColorView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  ColorChange
+//
+//  Created by Walbeck, Cooper on 8/24/22.
+//
+
+import SwiftUI
+
+struct ColorView: View
+{
+    var body: some View
+    {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider
+{
+    static var previews: some View
+    {
+        ColorView()
+    }
+}
---
+++ ColorChange/Resources/ColorChangeApp.swift
@@ -0,0 +1,20 @@
+//
+//  ColorChangeApp.swift
+//  ColorChange
+//
+//  Created by Walbeck, Cooper on 8/24/22.
+//
+
+import SwiftUI
+
+@main
+struct ColorChangeApp: App
+{
+    var body: some Scene
+    {
+        WindowGroup
+        {
+            ColorView()
+        }
+    }
+}
